361ba7014b8be2c38d9e10df432f86b2be301880
ArteeemPonomarev <artponomarev1994@gmail.com>
2021-02-18 01:27:14 +0300

    first commit, 13 lesson

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Header = () => {
+  return (
+    <header className="header">
+        <img src="https://seafood-tip.com/uploads/2019/04/logo-30x30.png" alt="logo" />
+      </header>
+  )
+};
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const Profile = () => {
+    return (
+        <main className="mainContent">
+            <img src="https://fotorelax.ru/wp-content/uploads/2016/03/Beautiful-photos-and-pictures-on-various-subjects-01-1024x640.jpg" alt="background picture" />
+          Main content
+            <div>
+                ava+description
+       </div>
+            <div>
+                My Posts
+       </div>
+            <div>post1</div>
+            <div>post2</div>
+        </main>
+    )
+};
+
+export default Profile;
